67d208b2bd16fdcd122447690862d985b20e53e7
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-06-23 17:57:15 +0530

    adding crud frontend url

--- crud_backend/src/main/java/com/sundar_java/crud_backend/controller/UserController.java
+++ crud_backend/src/main/java/com/sundar_java/crud_backend/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://crud-frontend-sandy.vercel.app")
 public class UserController {
 
     @Autowired
