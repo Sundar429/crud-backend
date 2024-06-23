56ac2f81651b965eaa4c95bf292050d7b1c4879e
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-06-23 20:38:02 +0530

    editing in  webconfig

--- crud_backend/src/main/java/com/sundar_java/crud_backend/config/WebConfig.java
+++ crud_backend/src/main/java/com/sundar_java/crud_backend/config/WebConfig.java
@@ -2,25 +2,52 @@ package com.sundar_java.crud_backend.config;
 
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig {
 
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://crud-frontend-sandy.vercel.app")
-                        .allowedMethods("GET"," POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
+    SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+
+        http.sessionManagement(management ->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .cors(cors->cors.configurationSource(corsConfigrationSource()));
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigrationSource() {
+
+        return request -> {
+            CorsConfiguration cfg=new CorsConfiguration();
+
+            cfg.setAllowedOrigins(Arrays.asList(
+                    "http://localhost:3000",
+                    "https://crud-frontend-sandy.vercel.app/"
+
+
+            ));
+            cfg.setAllowedMethods(Collections.singletonList("*"));
+            cfg.setAllowCredentials(true);
+            cfg.setAllowedHeaders(Collections.singletonList("*"));
+            cfg.setExposedHeaders(Arrays.asList("Authorization"));
+            cfg.setMaxAge(3600L);
+            return cfg;
         };
+
     }
 }
