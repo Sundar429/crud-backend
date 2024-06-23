f9caecd16e12c28200d9ca252aa50d6228ffe880
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-06-23 21:50:01 +0530

    addind config file

--- crud_backend/src/main/java/com/sundar_java/crud_backend/CrudBackendApplication.java
+++ crud_backend/src/main/java/com/sundar_java/crud_backend/CrudBackendApplication.java
@@ -2,12 +2,24 @@ package com.sundar_java.crud_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CrudBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudBackendApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurerAdapter corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/users").allowedOrigins("http://localhost:9000","https://crud-frontend-sandy.vercel.app");
+			}
+		};
 	}
 
 }
