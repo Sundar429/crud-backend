0214be0de60f9c2feae87686e26fe59d3091e074
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-06-23 23:08:07 +0530

    changing to first

--- crud_backend/src/main/java/com/sundar_java/crud_backend/CrudBackendApplication.java
+++ crud_backend/src/main/java/com/sundar_java/crud_backend/CrudBackendApplication.java
@@ -2,9 +2,8 @@ package com.sundar_java.crud_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
 
 @SpringBootApplication
 public class CrudBackendApplication {
@@ -12,14 +11,6 @@ public class CrudBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudBackendApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurerAdapter corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/users").allowedOrigins("http://localhost:3000","https://crud-frontend-sandy.vercel.app");
-			}
-		};
-	}
+
 
 }
--- crud_backend/src/main/java/com/sundar_java/crud_backend/controller/UserController.java
+++ crud_backend/src/main/java/com/sundar_java/crud_backend/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 
-@CrossOrigin("https://crud-frontend-sandy.vercel.app")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
