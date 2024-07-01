60dbe639027f26cbb060b8a6840e7bf24760e9be
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-07-01 20:04:47 +0530

    docker file added11

--- src/main/java/com/sundar_java/crud_backend_app/CrudBackendApplication.java
+++ src/main/java/com/sundar_java/crud_backend_app/CrudBackendApplication.java
@@ -1,8 +1,8 @@
-package com.sundar_java.crud_backend;
+package com.sundar_java.crud_backend_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
@@ -10,7 +10,18 @@ public class CrudBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudBackendApplication.class, args);
+
+
+
 	}
+	@GetMapping("/welecome")
+	public String sayHello(){
+		System.out.println("In the Spring boot main");
+		return "Hello Welecome to sprng boot world";
+	}
+
+
+
 
 
 }
